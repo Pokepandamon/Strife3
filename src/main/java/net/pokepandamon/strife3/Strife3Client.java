b2f9e72003da3c3578a3d4ee92c05c3345e2ed74
Pokepandamon <swanerikson@gmail.com>
2024-09-04 14:44:13 -0700

    Initial commit

---
+++ src/main/java/net/pokepandamon/strife3/Strife3Client.java
@@ -0,0 +1,10 @@
+package net.pokepandamon.strife3;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Strife3Client implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/pokepandamon/strife3/Strife3DataGenerator.java
@@ -0,0 +1,11 @@
+package net.pokepandamon.strife3;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Strife3DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
